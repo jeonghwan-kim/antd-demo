5c7b600be2be09c9bb8e62e1bd3dc921cef07c3e
김정환 <6pack@madup.com>
2018-10-08 13:35:28 +0900

    Change theme color

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,16 @@
 const { injectBabelPlugin } = require('react-app-rewired');
+const rewireLess = require('react-app-rewire-less');
 
 module.exports = function override(config, env) {
   config = injectBabelPlugin(
-    ['import', { libraryName: 'antd', libraryDirectory: 'es', style: 'css' }],
+    ['import', { libraryName: 'antd', libraryDirectory: 'es', style: true }], // less
     config,
   );
+
+  config = rewireLess.withLoaderOptions({
+    modifyVars: { "@primary-color": "#1DA57A" },
+    javascriptEnabled: true,
+  })(config, env);
+
   return config;
 };
